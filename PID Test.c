3e61408f882baec3f978d78c7c8f9ff3b03e5773
SquidCaptain <35849316+SquidCaptain@users.noreply.github.com>
2018-01-26 11:15:35 -0800

    Add files via upload

---
+++ PID Test.c
@@ -0,0 +1,8 @@
+#pragma config(Sensor, dgtl1,  myEncoder,      sensorQuadEncoder)
+#pragma config(Motor,  port1,           myMotor,       tmotorVex393TurboSpeed_HBridge, openLoop)
+
+task main()
+{
+}
+
+//that ^ or just use this https://www.vexforum.com/index.php/24458-built-in-pid-and-movemotortarget/0
