54dedc7343ad6bfe4557c5a0b34d5d0104f73f31
Jaimin Nimavat <jaimin2468@gmail.com>
2018-02-25 13:58:58 -0800

    Add files via upload

--- autoSkillsComp.c
+++ autoSkillsComp.c
@@ -1,17 +1,21 @@
 #pragma config(Sensor, in1,    lineLeft,       sensorLineFollower)
 #pragma config(Sensor, in2,    lineCenter,     sensorLineFollower)
 #pragma config(Sensor, in3,    lineRight,      sensorLineFollower)
-#pragma config(Sensor, in4,    gyro,           sensorGyro)
+#pragma config(Sensor, in4,    lineFront,      sensorLineFollower)
+#pragma config(Sensor, in5,    lineBack,       sensorLineFollower)
+#pragma config(Sensor, in6,    gyroLeft,       sensorGyro)
+#pragma config(Sensor, in7,    gyroRight,      sensorGyro)
 #pragma config(Sensor, dgtl1,  encoderMogo,    sensorQuadEncoder)
 #pragma config(Sensor, dgtl3,  encoderRight,   sensorQuadEncoder)
 #pragma config(Sensor, dgtl5,  encoderLeft,    sensorQuadEncoder)
-#pragma config(Motor,  port2,           backR,         tmotorVex393TurboSpeed_MC29, openLoop, reversed)
-#pragma config(Motor,  port3,           backL,         tmotorVex393TurboSpeed_MC29, openLoop)
-#pragma config(Motor,  port4,           tipR,          tmotorVex393HighSpeed_MC29, openLoop)
+#pragma config(Motor,  port1,           test,          tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port2,           frontR,        tmotorVex393TurboSpeed_MC29, openLoop, reversed)
+#pragma config(Motor,  port3,           midR,          tmotorVex393TurboSpeed_MC29, openLoop, reversed)
+#pragma config(Motor,  port4,           backR,         tmotorVex393TurboSpeed_MC29, openLoop, reversed)
 #pragma config(Motor,  port5,           mogoR,         tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port6,           mogoL,         tmotorVex393_MC29, openLoop, reversed)
-#pragma config(Motor,  port7,           tipL,          tmotorVex393HighSpeed_MC29, openLoop)
-#pragma config(Motor,  port8,           frontR,        tmotorVex393TurboSpeed_MC29, openLoop, reversed)
+#pragma config(Motor,  port7,           backL,         tmotorVex393TurboSpeed_MC29, openLoop)
+#pragma config(Motor,  port8,           midL,          tmotorVex393TurboSpeed_MC29, openLoop)
 #pragma config(Motor,  port9,           frontL,        tmotorVex393TurboSpeed_MC29, openLoop)
 
 // This code is for the VEX cortex platform
@@ -24,12 +28,10 @@
 #include "Vex_Competition_Includes.c"
 
 int front = 1;
-int back  = -1;
-/*
-float kp = 5;
-float ki = 5;
-float kd = 5;
-*/
+int back	= -1;
+int right = 1;
+int left = -1;
+
 int MAX_POWER = 121;
 int MIN_POWER = 40;
 int MAX_ERROR = 50;
@@ -37,8 +39,12 @@ int MAX_ERROR = 50;
 int g = 0;
 int motorEncoderTest = 0;
 
+float gyroScale = 1;
+int mogoTick = 0;
+int whiteLine = 0;
+
 /*/////////////////////////////*/
-/*       Reset Functions       */
+/*			 Reset Functions			 */
 /*/////////////////////////////*/
 
 void resetDrive()
@@ -69,145 +75,164 @@ void resetMogoEncoder()
 }
 
 /*/////////////////////////////*/
-/*    Basic Drive Functions    */
+/*		Basic Drive Functions		 */
 /*/////////////////////////////*/
 
-void move(float distance, int speed)//distance controls direction not spd
-{
-	float spd = speed;
-	float leftSpd = spd;
-	float rightSpd = spd;
-	float inverter = 1.0;
-	if(distance < 0.0)
-	{
-		inverter *= -1.0;
-	}
-	float cir = 4.0*PI; // 12.56
 
-	SensorValue[encoderRight] = 0;
-	SensorValue[encoderLeft] = 0;
-	while(abs(distance) > abs(SensorValue[encoderRight]) && abs(distance) > abs(SensorValue[encoderLeft]))
+void move(float targetDistance, int maxPower)
+{
+	float kp = 0.75;
+	float ki = 0;
+	float kd = 0;
+
+	/* Variables used to calculate how fast bot is moving*/
+	float wheelDiameter = 4.5;
+	float circumference = wheelDiameter * PI; //Roughly 14.13 inches
+	float currentDistance = 0;
+	float error = 1;
+	float lastError = 0;
+	float integral = 0;
+	float derivative = 0;
+	int power = 0;
+	int powerLeft = 0;
+	int powerRight = 0;
+
+	resetDriveEncoder();
+
+	while(error != 0)
 	{
-		rightSpd = spd;
-		leftSpd = spd;
-		//uhhhhh idk if this works but whatever
-		if(leftSpd > 125.0){
-			leftSpd = 125.0;
-		}
-		else if(leftSpd < -125.0)
+		currentDistance = SensorValue[encoderRight];
+
+		error = targetDistance - currentDistance;
+
+		integral = (error > MAX_ERROR ) ? 0 : (integral + error);
+		derivative = error - lastError;
+		lastError = error;
+
+		power = (error * kp) + (integral * ki) + (derivative * kd);
+
+		if(power > 0)
 		{
-			leftSpd = -125.0;
+			if(power > maxPower)
+			{
+				power = maxPower;
+			}
+			else if(power < MIN_POWER)
+			{
+				power = MIN_POWER;
+			}
 		}
-		if(rightSpd > 125.0){
-			rightSpd = 125.0;
-		}
-		else if(rightSpd < -125.0)
+		else if(power < 0)
 		{
-			rightSpd = -125.0;
+			if(power < -maxPower)
+			{
+				power = -maxPower;
+			}
+			else if(power > -MIN_POWER)
+			{
+				power = -MIN_POWER;
+			}
 		}
-		motor[backL] = leftSpd * inverter;
-		motor[backR] = rightSpd * inverter;
-		motor[frontL] = leftSpd * inverter;
-		motor[frontR] = rightSpd * inverter;
+		powerLeft = power;
+		powerRight = power;
+
+		motor[frontL] = powerLeft;
+		motor[backL] = powerLeft;
+		motor[frontR] = powerRight;
+		motor[backR] = powerRight;
+
 	}
-	motor[backL] = -(leftSpd * inverter) / 3;
-	motor[backR] = -(rightSpd * inverter) / 3;
-	motor[frontL] = -(leftSpd * inverter) / 3;
-	motor[frontR] = -(rightSpd * inverter) / 3;
-	wait1Msec(200);
-	motor[backL] = 0;
-	motor[backR] = 0;
-	motor[frontR] = 0;
-	motor[frontL] = 0;
+	resetDrive();
 }
 
-void turn(float bearing)//assume positive is right and negative is left
+int encoderPID(int power, int side)
 {
-	//bearing*=(4/5);
-	bearing = (bearing * 9) / 10;//6.755
-	float currentBearing = SensorValue[gyro]/10;
-	float targetBearing = bearing + currentBearing;
-	float TURN_SLOWDOWN = 10;
+	int kp = 0;
 
-	//float gyr = SensorValue[Gyro]/10;
+	int master = 0;
+	int slave = 0;
+	int error = 1;
 
+	master = (SensorValue[encoderRight] >= SensorValue[encoderLeft]) ? SensorValue[encoderRight] : SensorValue[encoderLeft];
+	slave = (SensorValue[encoderRight] >= SensorValue[encoderLeft]) ? SensorValue[encoderLeft] : SensorValue[encoderRight];
 
-	//MAKE TURNING ACCURATE BY MAKING REVERSE FOR 80ms
+	error = master - slave;
 
-
-	if (targetBearing < SensorValue[gyro]/10) // Left turn
+	if(side == right)
 	{
-
-		if(targetBearing > 0 && targetBearing > 360)
+		if(SensorValue[encoderRight] >= SensorValue[encoderLeft])
 		{
-		targetBearing = targetBearing - 360;
+			power = power - error * kp;
 		}
-		else if(targetBearing < 0 && targetBearing < -360)
-		{
-		targetBearing = targetBearing + 360;
-		}
-
-		while (SensorValue[gyro]/10 > targetBearing + TURN_SLOWDOWN)
-		{
-			motor[frontL] = -100;
-			motor[backL] = -100;
-			motor[frontR] = 100;
-			motor[backR] = 100;
-			//gyr = SensorValue[Gyro]/10;
-		}
-		while (SensorValue[gyro]/10 > targetBearing)
-		{
-			motor[frontL] = -50;
-			motor[backL] = -50;
-			motor[frontR] = 50;
-			motor[backR] = 50;
-			//gyr = SensorValue[Gyro]/10;
-		}
-
-		motor[frontL] = 40;
-		motor[backL] = 40;
-		motor[frontR] = -40;
-		motor[backR] = -40;
-		wait1Msec(200);
-
-		resetDrive();
 	}
-	else // Right turn
+	else if(side == left)
 	{
-
-		if(targetBearing > 0 && targetBearing > 360)
+		if(SensorValue[encoderRight] < SensorValue[encoderLeft])
 		{
-		targetBearing = targetBearing - 360;
+			power = power + error * kp;
 		}
-		else if(targetBearing < 0 && targetBearing < -360)
+	}
+	return power;
+}
+
+void turn(float bearing)
+{
+	float kp = 0;
+	float ki = 0;
+	float kd = 0;
+
+	float integral = 0;
+	float derivative = 0;
+
+	float lastError = 0;
+	float power = 0;
+	int powerLeft = 0;
+	int powerRight = 0;
+
+	float error = 1;
+	float currentBearing = (SensorValue[gyroLeft] + SensorValue[gyroRight]) / 2 * gyroScale;
+	float targetBearing = bearing * 10 + currentBearing;
+
+	while(error != 0)
+	{
+		error = targetBearing - currentBearing * gyroScale;
+		integral = (error > MAX_ERROR / ki) ? 0 : (integral + error);
+		derivative = error - lastError;
+
+		lastError = error;
+
+		power = (error * kp) + (integral * ki) + (derivative * kd);
+
+		if(power > 0)
 		{
-		targetBearing = targetBearing + 360;
+			if(power > MAX_POWER)
+			{
+				power = MAX_POWER;
+			}
+			else if(power < MIN_POWER)
+			{
+				power = MIN_POWER;
+			}
 		}
 
-		while (SensorValue[gyro]/10 < targetBearing - TURN_SLOWDOWN)
+		else if(power < 0)
 		{
-			motor[frontL] = 100;
-			motor[backL] = 100;
-			motor[frontR] = -100;
-			motor[backR] = -100;
-			//gyr = SensorValue[Gyro]/10;
+			if(power < -MAX_POWER)
+			{
+				power = -MAX_POWER;
+			}
+			else if(power > -MIN_POWER)
+			{
+				power = -MIN_POWER;
+			}
 		}
-		while (SensorValue[gyro]/10 < targetBearing)
-		{
-			motor[frontL] = 50;
-			motor[backL] = 50;
-			motor[frontR] = -50;
-			motor[backR] = -50;
-			//gyr = SensorValue[gyro]/10;
-		}
-		motor[frontL] = -40;
-		motor[backR] = -40;
-		motor[frontR] = 40;
-		motor[backR] = 40;
+		powerLeft = (error >= 0) ? power : -power;
+		powerRight = (error >= 0) ? -power : power;
 
-		wait1Msec(200);
-
-		resetDrive();
+		motor[frontL] = powerLeft;
+		motor[backL] = powerLeft;
+		motor[frontR] = powerRight;
+		motor[backR] = powerRight;
 	}
 }
 
@@ -218,7 +243,7 @@ void mogo(int choice)
 	//lower mogo intake
 	if(choice == front)
 	{
-		while(abs(SensorValue[encoderMogo]) <= 95)
+		while(abs(SensorValue[encoderMogo]) <= mogoTick)
 		{
 			motor[mogoL] = 121;
 			motor[mogoR] = 121;
@@ -229,7 +254,7 @@ void mogo(int choice)
 	//raise mogo intake
 	else if(choice == back)
 	{
-		while(abs(SensorValue[encoderMogo]) <= 95)
+		while(abs(SensorValue[encoderMogo]) <= mogoTick)
 		{
 			motor[mogoL] = -121;
 			motor[mogoR] = -121;
@@ -239,13 +264,21 @@ void mogo(int choice)
 	}
 }
 
-
-
-/*/////////////////////////////*/
-/*    Placing in Point Zones   */
-/*/////////////////////////////*/
-
-
+void lineTurn(int choice)
+{
+	while(SensorValue[lineFront] != whiteLine && SensorValue[lineCenter] != whiteLine && SensorValue[lineBack] != whiteLine)
+	{
+		if(SensorValue[lineLeft] == whiteLine)
+		{
+			turn(3);
+		}
+		else if(SensorValue[lineRight] == whiteLine)
+		{
+			turn(-3);
+		}
+	}
+	resetDrive();
+}
 
 void pre_auton()
 {
@@ -254,18 +287,21 @@ void pre_auton()
 	resetDriveEncoder();
 	resetMogoEncoder();
 
-	SensorType[gyro] = sensorNone;
+	SensorType[gyroRight] = sensorNone;
+	SensorType[gyroLeft] = sensorNone;
 	wait1Msec(500);
-	SensorType[gyro] = sensorGyro;
-	wait1Msec(1100);	
+	SensorType[gyroRight] = sensorGyro;
+	SensorType[gyroLeft] = sensorGyro;
+	wait1Msec(1100);
 }
 
 task autonomous()
 {
-	mogo(front);
+	move(902, 121);
+	/*mogo(front);
 	move(425, 121);
 	move(20, 70);
-	
+
 	mogo(back);
 	wait1Msec(100);
 
@@ -284,10 +320,10 @@ task autonomous()
 	constantDrive(0);
 
 	move(-242, 121);
-	
+
 	move(80, 70);
 	move(-80, 70);
-	
+
 	turn(-86);
 	move(192, 121);
 
@@ -300,9 +336,10 @@ task autonomous()
 	move(-300, 121);
 
 	turn(168);
-	move(front);
+	mogo(front);
 	move(80, 121);
 	move(-80, 121);
+	*/
 }
 
 int threshold = 8;
@@ -319,21 +356,21 @@ void deadZoneCheck()
 	channel3 = (abs(vexRT[Ch3]) > threshold) ? vexRT[Ch3] : 0;
 	channel4 = (abs(vexRT[Ch4]) > threshold) ? vexRT[Ch4] : 0;
 
-	if(channel2 >= 121)
+	if(channel2 >= 101)
 	{
-		channel2 = 120;
+		channel2 = 100;
 	}
-	else if(channel2 <= -121)
+	else if(channel2 <= -101)
 	{
-		channel2 = -120;
+		channel2 = -100;
 	}
-	if(channel3 >= 121)
+	if(channel3 >= 101)
 	{
-		channel3 = 120;
+		channel3 = 100;
 	}
-	else if(channel3 <= -121)
+	else if(channel3 <= -101)
 	{
-		channel3 = -120;
+		channel3 = -100;
 	}
 }
 
@@ -342,10 +379,13 @@ task usercontrol()
 	// User control code here, inside the loop
 	while (true)
 	{
-		g = SensorValue[gyro];
+		g = SensorValue[gyroLeft];
+		int g2 = SensorValue[gyroRight];
 		motorEncoderTest = SensorValue[encoderMogo];
 
-		if(vexRT[Btn7U]  == 1)
+		motor[test] = 100;
+
+		if(vexRT[Btn7U]	 == 1)
 		{
 			motor[backL] = motorType[NULL];
 			motor[backR] = motorType[NULL];
@@ -363,8 +403,10 @@ task usercontrol()
 
 		//drive motors
 		motor[frontL] = channel3;
+		motor[midL] = channel3;
 		motor[backL] = channel3;
 		motor[frontR] = channel2;
+		motor[midR] = channel2;
 		motor[backR] = channel2;
 
 		//mobile lift goes up on button 6U
@@ -385,7 +427,18 @@ task usercontrol()
 			motor[mogoL] = 0;
 			motor[mogoR] = 0;
 		}
+		if(SensorValue[encoderMogo] == 95)
+		{
+			motor[mogoL] = 0;
+			motor[mogoR] = 0;
+		}
 
+		if(SensorValue[encoderMogo] == 0)
+		{
+			motor[mogoL] = 0;
+			motor[mogoR] = 0;
+		}
+		/*
 		if(vexRT[Btn8D] == 1)
 		{
 			motor[tipL] = -23;
@@ -410,5 +463,6 @@ task usercontrol()
 			motor[tipL] = 0;
 			motor[tipR] = 0;
 		}
+		*/
 	}
 }
